c9d133fd4db6e6efb489a7071301b5a72f9eb1ef
Afshin Mokhtari <afshinator@hotmail.com>
2014-01-08 08:20:33 +0000

    end of branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }     # email is indexed in db, indexes should be lowercase
   
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i    # original
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i    # disallow double dots  
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
           uniqueness: { case_sensitive: false }
   
