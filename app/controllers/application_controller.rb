7f3bd87063135d303c002d82f31f497705aabbe2
Afshin Mokhtari <afshinator@hotmail.com>
2014-01-18 10:26:47 +0000

    start sign in out specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+    
+  # including it here makes it available to all controllers and views
+  include SessionsHelper
 end
